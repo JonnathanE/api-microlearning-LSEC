a6409643cc1a56f57a38a977d6b150bf2070e29e
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2021-07-06 02:28:26 -0500

    check id user id admin
    create method in authJwt.js middleware to check if user is admin

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -18,5 +18,13 @@ exports.isModerator = async (req, res, next) => {
 }
 
 exports.isAdmin = async (req, res, next) => {
-
+    const user = await User.findById(req.userId);
+    const roles = await Role.find({ _id: { $in: user.roles } });
+    for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === 'admin') {
+            next();
+            return;
+        }
+    }
+    return res.status(403).json({ message: 'Requiere rol de administrador' });
 }
