2ae5bc449c6adca5cef8085676015305e4065dd3
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2021-07-06 02:25:29 -0500

    check if user is moderator
    create method in authJwt.js middleware to check if user is moderator

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -1,3 +1,4 @@
+const User = require('../models/User');
 const Role = require('../models/Role');
 
 exports.verifyToken = async (req, res, next) => {
@@ -5,7 +6,15 @@ exports.verifyToken = async (req, res, next) => {
 }
 
 exports.isModerator = async (req, res, next) => {
-
+    const user = await User.findById(req.userId);
+    const roles = await Role.find({ _id: { $in: user.roles } });
+    for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === 'moderator') {
+            next();
+            return;
+        }
+    }
+    return res.status(403).json({ message: 'Requiere rol de moderador' });
 }
 
 exports.isAdmin = async (req, res, next) => {
