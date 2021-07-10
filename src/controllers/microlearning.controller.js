a1efd58906b7ad8ba8e13328e5ae56ef68d09cd2
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2021-07-10 17:32:36 -0500

    create and get all microlearning
    create the model in the mongoose anda create the funcion for create and get all microlearning

---
+++ src/controllers/microlearning.controller.js
@@ -0,0 +1,55 @@
+'use strict';
+const formidable = require('formidable');
+const fs = require('fs');
+
+const Micro = require('../models/Microlearning');
+const { errorHandler } = require('../helpers/dberrorHandler')
+
+class Microlearning {
+    create = async (req, res) => {
+        let form = new formidable.IncomingForm();
+        form.keepExtensions = true;
+        form.parse(req, async (err, fields, files) => {
+
+            if (err) return res.status(400).json({ error: 'No se pudo cargar la imagen' });
+
+            let micro = new Micro(fields);
+
+            if (files.image) {
+                if (files.image.size > 9000000) {
+                    return res.status(400).json({ error: 'La imagen debe tener un tamaño inferior a 9 MB.' });
+                }
+                micro.image.data = fs.readFileSync(files.image.path);
+                micro.image.contentType = files.image.type;
+            }
+
+            if (files.gif) {
+                if (files.image.size > 9000000) {
+                    return res.status(400).json({ error: 'El GIF debe tener un tamaño inferior a 9 MB.' });
+                }
+                micro.gif.data = fs.readFileSync(files.image.path);
+                micro.gif.contentType = files.gif.type;
+            }
+
+            await micro.save((err, result) => {
+                if (err) return res.status(400).json({ error: errorHandler(err) });
+                res.json(result);
+            });
+        });
+    }
+
+    getAll = async (req, res) => {
+        let order = req.query.order ? req.query.order : 'asc'; // variable to sort the results; ascending by default
+        let sortBy = req.query.sortBy ? req.query.sortBy : 'title'; // filter
+
+        await Micro.find()
+            .select(['-image', '-gif'])
+            .sort([[sortBy, order]])
+            .exec((err, micro) => {
+                if (err) return res.status(400).json({ error: 'Microcontenidos no encontrados' });
+                res.status(200).json(micro);
+            });
+    }
+}
+
+module.exports = Microlearning;
---
+++ src/routes/microlearning.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const controllersFactory = require('../controllers/controllersFactory');
+const { verifyToken, isAdmin, isModerator } = require('../middlewares/authJwt');
+
+const factory = new controllersFactory();
+const micro = factory.getController('microlearning');
+
+// create microlearning
+router.post('/', micro.create);
+// get the whole microlearning without the image and gif
+router.get('/', micro.getAll);
+
+module.exports = router;
