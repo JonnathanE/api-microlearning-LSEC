98bffe47c5c01c4966ac3e43de1c1499e54976d4
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2021-08-04 05:16:51 -0500

    create method for show modules and lesson in interfaz student

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -2,7 +2,7 @@ const Lessons = require('../models/Lesson');
 const Microlearning = require('../models/Microlearning');
 const Modules = require('../models/Module');
 
-exports.modulesAssigned= async (req, res) => {
+exports.modulesAssigned = async (req, res) => {
     try {
         const modules = await Modules.find().sort({ number: 1 });
         let reults = [];
@@ -14,6 +14,33 @@ exports.modulesAssigned= async (req, res) => {
         }
         return res.status(200).json(reults);
     } catch (error) {
-        return res.status(400).json({error: 'No se cargaron los datos. Recargar la página'});
+        return res.status(400).json({ error: 'No se cargaron los datos. Recargar la página' });
     }
+}
+
+exports.lessonsAssigned = async (req, res) => {
+    const module = await Modules.findById(req.params.moduleId);
+    if (!module) return res.status(400).json({ error: 'El modulo no se encontró o no está registrado' });
+    const lessons = await Lessons.find({ module: module._id }).select(['-icon']);
+    return res.status(200).json(lessons);
+}
+
+exports.lessonById = async (req, res, next, id) => {
+    await Lessons.findById(id)
+        .populate("module")
+        .exec((err, lesson) => {
+            if (err || !lesson) {
+                return res.status(400).json({ error: "La lección no se encontró o no existe" });
+            }
+            req.lesson = lesson;
+            next();
+        });
+}
+
+exports.moduleById = async (req, res, next, id) => {
+    await Modules.findById(id).exec((err, data) => {
+        if (err || !data) return res.status(400).json({ error: 'El modulo no se encontró o no existe' });
+        req.module = data;
+        next();
+    });
 }
--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { verifyToken, isAdmin, isModerator } = require('../middlewares/authJwt');
-const { modulesAssigned } = require('../controllers/home.controller');
+const { modulesAssigned, lessonsAssigned } = require('../controllers/home.controller');
 
 router.get('/modules', modulesAssigned);
+router.get('/lessons/:moduleId', lessonsAssigned);
+
+// method to obtain the parameter
+//router.param('lessonId', lesson.byId)
 
 module.exports = router;
